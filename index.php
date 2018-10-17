452669f09dc1042fa3695e38a018dbd75e6e96a4
Lucas Gerrits <gerril39@uwosh.edu>
2018-10-17 16:00:53 -0500

    Initial Push
    Base code of functional static pages.

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+	require('header.php');
+?>
+
+<section>
+	<div class="section-header">
+		MEETING TIMES
+	</div>
+	<div class="section-body">
+		We meet every <span class="b">Thursday at 5:00 PM in Halsey 202</span>.
+	</div>
+</section>
+
+<section>
+	<div class="section-header">
+		MISSION STATEMENT
+	</div>
+	<div class="section-body">
+		The purpose of the CS Club is to establish a fun, creative environment where likeminded 
+		individuals can gather to share their love of computers and electronics, in whatever form
+		that may be.
+	</div>
+</section>
+
+<section>
+	<div class="section-header">
+		FALL 2018 EXEC BOARD
+	</div>
+	<div class="section-body">
+		<span class="b">President:</span> Lucas Gerrits - gerril39@uwosh.edu<br>
+		<span class="b">Vice-President:</span> Jacob Hein - heinj82@uwosh.edu<br>
+		<span class="b">Treasurer:</span> Robbie Freiberg - freibr51@uwosh.edu<br>
+		<span class="b">Student Advisor:</span> Justin Espiritu - espirj96@uwosh.edu<br>
+		<br>
+		<span class="b">Faculty Advisor:</span> Erik Krohn - krohne@uwosh.edu<br>
+		<span class="b">Faculty Advisor:</span> Hannah Hillberg - hillbergh@uwosh.edu
+	</div>
+</section>
+
+
+<?php
+	require('footer.php');
+?>

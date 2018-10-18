5488988cfb61f24cb11d231f14ec8488a8afe53a
Lucas Gerrits <gerril39@uwosh.edu>
2018-10-17 21:51:10 -0500

    Fix Attempt 2
    Attempt 2

--- header.php
+++ header.php
@@ -1,58 +1,1 @@
-<?php
-/**
- * CS Club v1.0
- * @version 2018-10-09
- */ 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <title>University of Wisconsin-Oshkosh : Computer Science Club</title>
-   <meta charset="utf-8">
-   <meta name="description" content="News and useful information related to the University of Wisconsin-Oshkosh Computer Science Club.">
-   <meta name="keywords" content="UWO,UWOshkosh,Oshkosh,Computer Science,CS Club,UWO CS Club">
-   <meta name="author" content="UWO CS Club">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="theme-color" content="#FEC424">
-   <meta property="og:title" content="University of Wisconsin-Oshkosh : Computer Science Club">
-   <meta property="og:type" content="website">
-   <meta property="og:url" content="">
-   <meta property="og:image" content="">
-   <meta property="og:description" content="News and useful information related to the University of Wisconsin-Oshkosh Computer Science Club.">
-   <link rel="icon" href="favicon.ico">
-   <link rel="stylesheet" href="styles.css">
-   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-</head>
-<body>
-<header>
-	<div id="header">
-		<img src="img/CSIcon.png" id="logo" alt="UWO CS Icon">
-		<h1>University of Wisconsin-Oshkosh<br>Computer Science Club</h1>
-	</div>
-</header>
-<nav>
-	<ul>
-		<li>
-			<a href="index.php" alt="Home">Home</a>
-		</li>
-		<li>
-			<a href="logs.php" alt="Logs">Logs</a>
-		</li>
-		<li>
-			<a href="resources.php" alt="Resources">Resources</a>
-		</li>
-		<li>
-			<a href="https://github.com/UWO-CS-Club" alt="GitHub" target="_blank">
-				GitHub
-			</a>
-		</li>
-		<li>
-			<a href="https://discord.gg/ACxzu4k" alt="Discord" target="_blank">
-				Discord
-			</a>
-		</li>
-	</ul>
-</nav>
-
-<div id="wrapper">
+<?php/** * CS Club v1.0 * @version 2018-10-09 */?><!DOCTYPE html><html lang="en"><head>	<title>University of Wisconsin-Oshkosh : Computer Science Club</title>		<meta charset="utf-8">	<meta name="description" content="News and useful information related to the University of Wisconsin-Oshkosh Computer Science Club.">	<meta name="keywords" content="UWO,UWOshkosh,Oshkosh,Computer Science,CS Club,UWO CS Club">	<meta name="author" content="UWO CS Club">	<meta name="viewport" content="width=device-width, initial-scale=1.0">	<meta name="theme-color" content="#FEC424">	<meta property="og:title" content="University of Wisconsin-Oshkosh : Computer Science Club">	<meta property="og:type" content="website">	<meta property="og:url" content="">	<meta property="og:image" content="">	<meta property="og:description" content="News and useful information related to the University of Wisconsin-Oshkosh Computer Science Club.">	<link rel="icon" href="favicon.ico">	<link rel="stylesheet" href="styles.css">	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script></head><body><header>	<div id="header">		<img src="img/CSIcon.png" id="logo" alt="UWO CS Icon">		<h1>University of Wisconsin-Oshkosh<br>Computer Science Club</h1>	</div></header><nav>	<ul>		<li>			<a href="index.php" alt="Home">Home</a>		</li>		<li>			<a href="logs.php" alt="Logs">Logs</a>		</li>		<li>			<a href="resources.php" alt="Resources">Resources</a>		</li>		<li>			<a href="https://github.com/UWO-CS-Club" alt="GitHub" target="_blank">				GitHub			</a>		</li>		<li>			<a href="https://discord.gg/ACxzu4k" alt="Discord" target="_blank">				Discord			</a>		</li>	</ul></nav><div id="wrapper">
